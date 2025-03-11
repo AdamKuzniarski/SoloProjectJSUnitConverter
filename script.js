f09f6506df67e7a63685492d391295936dd268c0
Adam <adam.kuzniarski@gmx.de>
2025-03-11 16:36:14 +0100

    dark mode icons added

--- script.js
+++ script.js
@@ -6,18 +6,31 @@
 
 const inputEl = document.getElementById('input');
 const convBtn = document.getElementById('btn');
-const out1 = document.getElementById('mToFeet');
-const out2 = document.getElementById('lToGallons');
-const out3 = document.getElementById('kgToPounds');
+let out1 = document.getElementById('mToFeet');
+let out2 = document.getElementById('lToGallons');
+let out3 = document.getElementById('kgToPounds');
 
 convBtn.addEventListener('click', ()=>{
     let inputValue = inputEl.value 
+
     
     const metersToFt = (inputValue * 3.281).toFixed(3);
     const lToGallons = (inputValue * 0.264).toFixed(3);
     const kgToPounds = (inputValue * 2.204).toFixed(3);
     
-    out1.textContent = `${inputValue} meters = ${metersToFt} feet | ${metersToFt} feet = ${inputValue} meters`;
+    
+
+    if(inputValue == 1){
+        out1.textContent = `${inputValue} meter = ${metersToFt} foot | ${metersToFt} foot = ${inputValue} meter`;
+        out2.textContent = `${inputValue} liter = ${lToGallons} gallon | ${lToGallons} gallon = ${inputValue} liter`;
+        out3.textContent = `${inputValue} kilo = ${kgToPounds} pound | ${kgToPounds} pound = ${inputValue} kilo`;
+    }else{
+        out1.textContent = `${inputValue} meters = ${metersToFt} feet | ${metersToFt} feet = ${inputValue} meters`;
+        out2.textContent = `${inputValue} liters = ${lToGallons} gallons | ${lToGallons} gallons = ${inputValue} liters`;
+        out3.textContent = `${inputValue} kilos = ${kgToPounds} pounds | ${kgToPounds} pounds = ${inputValue} kilos`;
+    }
+
+    /* out1.textContent = `${inputValue} meters = ${metersToFt} feet | ${metersToFt} feet = ${inputValue} meters`;
     out2.textContent = `${inputValue} liters = ${lToGallons} gallons | ${lToGallons} gallons = ${inputValue} liters`;
-    out3.textContent = `${inputValue} kilos = ${kgToPounds} pounds | ${kgToPounds} pounds = ${inputValue} kilos`;
+    out3.textContent = `${inputValue} kilos = ${kgToPounds} pounds | ${kgToPounds} pounds = ${inputValue} kilos`; */
 })
